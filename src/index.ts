c1110fa8a02c14a5aff8ed59e9286710b48185c3
Juliano Ladeira <julianoladeira@gmail.com>
2020-05-03 22:53:48 -0300

    Added missing export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import Kachecker from './kachecker/kachecker'
+
 export { default as KabumConfig } from './sellers/KaBuM/models/kabum-config';
+export { default as TerabyteshopConfig } from './sellers/Terabyteshop/models/terabyteshop-config'
+
+export { Product } from './core/models/product'
 
 export default Kachecker;
